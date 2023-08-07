a158a9fe38d21f1b06a3e7021df7cce36804c5f8
David Tacoronte <davidtacoronte@Davids-MBP.attlocal.net>
2023-08-07 17:54:55 -0400

    started building out project in App.js - included api fetch for dogs and constructor for state

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,2 +1,31 @@
 import React from 'react';
 import axios from 'axios';
+
+const fetchDogs = (breed) => {
+    return axios.get(`https://dog.ceo/api/breed/${breed}/images`)
+    .then(res => res)
+    .catch(err => console.error('Check API for Error!'))
+}
+
+class App extends React.Component {
+    
+    constructor() {
+        console.log('constructor ran!')
+        super();
+        this.state = {
+            doggos: [],
+            breed: 'husky'
+        }
+    }
+
+    render() {
+        console.log('rendering :-)')
+        return (
+            <>
+            <h1>My App</h1>
+            </>
+        )
+    }
+}
+
+export default App;
