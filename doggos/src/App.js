80f379ef7797d271ed2eae94118bc1566afdea40
David Tacoronte <davidtacoronte@Davids-MacBook-Pro.local>
2023-08-08 12:19:07 -0400

    finished completing all componenets and functionality, CDM, CDM, SearchDogs.

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import SearchForm from './SearchForm';
 
 const fetchDogs = (breed) => {
     return axios.get(`https://dog.ceo/api/breed/${breed}/images`)
@@ -14,15 +15,47 @@ class App extends React.Component {
         super();
         this.state = {
             doggos: [],
-            breed: 'husky'
+            breed: ''
         }
+    }
+
+    componentDidMount() {
+        console.log('component mounted..')
+        fetchDogs(this.state.breed).then(res => {
+            this.setState({doggos: res.data.message});
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('component did update');
+        if (prevState.doggos !== this.state.doggos) {
+            console.log('the dogs have changed');
+            if (this.state.breed === 'chihuahua') {
+                console.log('eww')
+                fetchDogs('husky').then(res => {
+                    this.setState({doggos: res.data.message, breed: 'husky'})
+                })
+            }
+        }
+    }
+
+    searchDogs = dogName => {
+        console.log('search dogs');
+        fetchDogs(dogName).then(res => {
+            this.setState({doggos: res.data.message, breed: dogName});
+        });
     }
 
     render() {
         console.log('rendering :-)')
         return (
             <>
-            <h1>My App</h1>
+            <h1>Find your favorite Doggy!</h1>
+            <h4>**NO CHIHUAHUAS 😖**</h4>
+            <SearchForm searchDogs={this.searchDogs}/>
+            {this.state.doggos.map((dog, index) => (
+            <img width="200" src={dog} key={index} alt={dog} />
+            ))}
             </>
         )
     }
